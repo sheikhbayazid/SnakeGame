122787433447df4a47180bdf41bd6257afb35d7e
Sheikh Bayazid <sheikhbayazid@gmail.com>
2021-03-13 20:21:42 +0600

    Initial Commit

---
+++ SnakeGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SnakeGame
+//
+//  Created by Sheikh Bayazid on 3/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SnakeGame/SnakeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeGameApp.swift
+//  SnakeGame
+//
+//  Created by Sheikh Bayazid on 3/13/21.
+//
+
+import SwiftUI
+
+@main
+struct SnakeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
